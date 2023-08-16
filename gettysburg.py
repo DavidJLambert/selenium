423a410bf0d83dbda720c9ef08160e3316e2f96a
David J. Lambert <14700050+DavidJLambert@users.noreply.github.com>
2023-08-16 13:17:51 -0700

    Update gettysburg.py

--- gettysburg.py
+++ gettysburg.py
@@ -1,1 +1,11 @@
 print("Four score and seven years ago, our forefathers...")
+
+my_name = "   David Lambert    "
+print(my_name.lower())
+print(my_name.upper())
+print(len(my_name))
+print(my_name.lower().title())
+print(my_name.strip())
+
+my_string = "alpha,beta,gamma"
+print(my_string.split(","))
